66e2a92ca6e155c9991280a315d22acce05dab7f
Austin McKee <amckee3@gmail.com>
2015-11-09 23:19:28 -0500

    DataSprite created

--- src/structures/data/DataSprite.java
+++ src/structures/data/DataSprite.java
@@ -1,5 +1,20 @@
 package structures.data;
 
-public class DataSprite {
+import java.io.FileNotFoundException;
 
+import structures.IResource;
+
+public class DataSprite implements IResource {
+	
+	private final String myName;
+	
+	public DataSprite(String name) {
+		myName = name;
+	}
+
+	@Override
+	public void load(String resourceFolder) throws FileNotFoundException {
+		
+	}
+	
 }
