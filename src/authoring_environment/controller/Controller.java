4158817273c80df69e7e55591f214fe7d69abbc9
steve <sam98@duk.edu>
2015-11-12 14:49:33 -0500

    started front end

---
+++ src/authoring_environment/controller/Controller.java
@@ -0,0 +1,10 @@
+package authoring_environment.controller;
+
+import structures.data.DataGame;
+
+public class Controller {
+	private DataGame myGame;
+	public Controller(){
+		// myGame = new DataGame("GAME");
+	}
+}
