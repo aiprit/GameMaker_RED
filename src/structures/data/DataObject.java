a9a9c3ca44b71f23c130fa098ac1c914ec3a45ba
Austin McKee <amckee3@gmail.com>
2015-11-11 22:24:05 -0500

    changed data object

--- src/structures/data/DataObject.java
+++ src/structures/data/DataObject.java
@@ -4,21 +4,15 @@ import java.awt.geom.Point2D;
 import java.util.Map;
 
 public class DataObject {
+	
     Map<DataEvent, DataObject> actions;
-    String compiledGroovyScript;
-    String objectTitle;
-    String spriteImage;
+    String name;
+    String sprite;
 
     Point2D position;
-    Point2D velocity;
-    double heading, angularVelocity;
+    double direction, angularVelocity;
     double scaleX, scaleY, alpha;
-
+    
     boolean visible;
-    boolean justCreated;
-    boolean markedForDestruction;
-
-    int ID;
-
     int zIndex;
 }
