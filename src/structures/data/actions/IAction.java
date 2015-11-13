50e5b597d77a4a00b846a1d364d1caf506870d58
Austin McKee <amckee3@gmail.com>
2015-11-13 00:51:47 -0500

    Created parameters (again)

--- src/structures/data/actions/IAction.java
+++ src/structures/data/actions/IAction.java
@@ -2,8 +2,11 @@ package structures.data.actions;
 
 import java.util.List;
 
+import exceptions.CompileTimeException;
 import structures.data.actions.params.IParameter;
 
 public interface IAction {
 	public List<IParameter> getParameters();
+	public String getName();
+	public String compileSyntax() throws CompileTimeException;
 }
--- src/testing/EngineTest.java
+++ src/testing/EngineTest.java
@@ -1,15 +1,8 @@
 package testing;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import structures.data.actions.params.IParameter;
-
 public class EngineTest {
 	public static void main(String[] args) {
-		List<IParameter.type> myParams = new ArrayList<>();
-		myParams.add(IParameter.type.DOUBLE);
-		myParams.add(IParameter.type.INTEGER);
-		
+		boolean bool = true;
+		System.out.println(bool);
 	}
 }
