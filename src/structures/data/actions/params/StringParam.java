70dd5f501b36dcdff29e4c7fdd73209f26fb4e7d
Austin McKee <amckee3@gmail.com>
2015-11-13 00:51:56 -0500

    what?

---
+++ src/structures/data/actions/params/StringParam.java
@@ -0,0 +1,25 @@
+package structures.data.actions.params;
+
+import exceptions.ParameterParseException;
+
+public class StringParam implements IParameter {
+	
+	private String myValue;
+	private String myTitle;
+
+	@Override
+	public void parse(String string) throws ParameterParseException {
+		myValue = string;
+	}
+
+	@Override
+	public String getOriginal() {
+		return myValue;
+	}
+	
+	@Override
+	public String getTitle() {
+		return myTitle;
+	}
+	
+}
