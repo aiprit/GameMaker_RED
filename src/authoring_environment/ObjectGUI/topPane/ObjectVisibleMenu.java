0adfcfd0b0a70208a3afc6d0c60b27a191686008
Nicky Groszewski <groszewn@gmail.com>
2015-11-13 22:07:12 -0500

    top pane

--- src/authoring_environment/ObjectGUI/topPane/ObjectVisibleMenu.java
+++ src/authoring_environment/ObjectGUI/topPane/ObjectVisibleMenu.java
@@ -1,5 +1,24 @@
 package authoring_environment.ObjectGUI.topPane;
 
-public class ObjectVisibleMenu {
+import java.util.ResourceBundle;
 
+import javafx.scene.control.Menu;
+import structures.data.DataGame;
+
+public class ObjectVisibleMenu {
+	ResourceBundle r = ResourceBundle.getBundle("authoring_environment/ObjectGUI/topPane/TopPaneResources");
+
+	public Menu makeMenu(DataGame game) {
+		Menu image = new Menu(r.getString("visibleTitle"));
+//		List<String> imageOptions = new ArrayList<String>();
+//		imageOptions.add(r.getString("imageItem1"));
+//		imageOptions.add(r.getString("imageItem2"));
+//		imageOptions.add(r.getString("imageItem3"));
+//		UpdateImage imageChanger = new UpdateImage(null, null);
+//		addMenuItem(image, imageOptions);
+//		for (MenuItem m : image.getItems()) {
+//			m.setOnAction((event) -> imageChanger.refreshImage(m.getText()));
+//		}
+		return image;
+	}
 }
