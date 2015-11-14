db44a76d3ece39cba62a762a34f2ef6f001486e1
Nicky Groszewski <groszewn@gmail.com>
2015-11-13 21:50:33 -0500

    Object GUI

--- src/authoring_environment/EventPopup.java
+++ src/authoring_environment/EventPopup.java
@@ -1,5 +1,18 @@
 package authoring_environment;
 
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 public class EventPopup {
+
+	public void popup() {
+		final Stage dialog = new Stage();
+		dialog.initModality(Modality.APPLICATION_MODAL);
+		VBox dialogVbox = new VBox(20);
+		dialogVbox.getChildren().add(new Text("This is a Dialog"));
+		Scene dialogScene = new Scene(dialogVbox, 300, 200);
+		dialog.setScene(dialogScene);
+		dialog.show();
+	}
 
 }
