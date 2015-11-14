0b01b6f650187dc5a3166ccc256142de0989c684
Parit Burintrathikul <aiprit@gmail.com>
2015-11-14 13:01:20 -0500

    change to textarea

--- src/authoring_environment/Action/GUI/ActionGUI.java
+++ src/authoring_environment/Action/GUI/ActionGUI.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -28,8 +29,7 @@ public class ActionGUI {
 	}
 	public void init() {
 		myStage.setTitle(r.getString("title"));
-		TextField text = new TextField(r.getString("textfield"));
-		text.setAlignment(Pos.TOP_LEFT);
+		TextArea text = new TextArea(r.getString("textfield"));
 		text.setMinWidth(Integer.parseInt(r.getString("screenWidth")));
 		text.setMinHeight(Integer.parseInt(r.getString("screenHeight")));
 		myRoot.getChildren().add(text);
