63f40f94e420d8fec5c9f0a065c3235ea9fcb0ce
Parit Burintrathikul <aiprit@gmail.com>
2015-11-13 21:47:51 -0500

    Rightpane

--- src/Launcher.java
+++ src/Launcher.java
@@ -17,6 +17,9 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import structures.data.DataObject;
+import structures.data.events.AKeyEvent;
+import structures.data.events.KeyTypedEvent;
 
 /**
  * @author loganrooper
@@ -31,42 +34,46 @@ public class Launcher extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-
-		primaryStage.setTitle("VOOGASalad Launcher");
-		Button btn = new Button();
-		btn.setText("Make/Edit Game");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-
-				//TODO: Open the Authoring Environment here
-			}
-		});
-		Button btn1 = new Button();
-		btn1.setText("Play Game");
-		btn1.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				try {
-					ec = new EngineController(primaryStage);
-				} catch (ResourceFailedException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-
-		Pane root = new Pane();
-		HBox a = new HBox(5);
-		Insets pad = new Insets(10, 10, 10, 10);
-
-		a.setPadding(pad);
-		btn.setPadding(pad);
-		btn1.setPadding(pad);
-		root.getChildren().add(a);
-		a.getChildren().addAll(btn, btn1);
-		primaryStage.setScene(new Scene(root));
-		primaryStage.show();
+		DataObject object = new DataObject("dog",10,10);
+		object.addEvent(new KeyTypedEvent());
+		ObjectGUI og = new ObjectGUI(object);
+		og.init();
+//
+//		primaryStage.setTitle("VOOGASalad Launcher");
+//		Button btn = new Button();
+//		btn.setText("Make/Edit Game");
+//		btn.setOnAction(new EventHandler<ActionEvent>() {
+//
+//			@Override
+//			public void handle(ActionEvent event) {
+//
+//				//TODO: Open the Authoring Environment here
+//			}
+//		});
+//		Button btn1 = new Button();
+//		btn1.setText("Play Game");
+//		btn1.setOnAction(new EventHandler<ActionEvent>() {
+//
+//			@Override
+//			public void handle(ActionEvent event) {
+//				try {
+//					ec = new EngineController(primaryStage);
+//				} catch (ResourceFailedException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//
+//		Pane root = new Pane();
+//		HBox a = new HBox(5);
+//		Insets pad = new Insets(10, 10, 10, 10);
+//
+//		a.setPadding(pad);
+//		btn.setPadding(pad);
+//		btn1.setPadding(pad);
+//		root.getChildren().add(a);
+//		a.getChildren().addAll(btn, btn1);
+//		primaryStage.setScene(new Scene(root));
+//		primaryStage.show();
 	}
 }
--- src/authoring_environment/ObjectGUI/ObjectRightPane.java
+++ src/authoring_environment/ObjectGUI/ObjectRightPane.java
@@ -11,15 +11,21 @@ public class ObjectRightPane {
 	private DataObject o;
 	private ObservableList<IDataEvent> list;
 	public ObjectRightPane(DataObject object) {
+		try{
 			o = object;
 			list = object.getEvents();
+		}
+		catch(NullPointerException e){
+
+		}
 	}
 
 	public Group init() {
 		Group root = new Group();
-		ListView<String> listview = new ListView<String>();
-		root.getChildren().addAll(list);
+		ListView<IDataEvent> listview = new ListView<IDataEvent>();
 		listview.setItems(list);
+		root.getChildren().addAll(listview);
+
 		return root;
 	}
 }
