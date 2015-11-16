9f29693b09f156687ab5556a1452188c473d2dbb
steve <sam98@duk.edu>
2015-11-16 13:13:19 -0500

    refactored view and now open an object editor

---
+++ src/authoring_environment/ObjectListWindow.java
@@ -0,0 +1,73 @@
+package authoring_environment;
+
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import authoring_environment.ObjectGUI.ObjectController;
+import authoring_environment.ObjectGUI.ObjectGUI;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import structures.IObject;
+import structures.data.DataObject;
+
+public class ObjectListWindow {
+	private static String NEW_ITEM = "MakeNewItem";
+	private static String EDIT_ITEM = "EditItem";
+	private static String OBJECT_TITLE = "ObjectListTitle";
+	public void init(BorderPane bp, Stage s, ResourceBundle resources){
+		EventHandler<ActionEvent> sButtonClick = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event){			 	
+					IObject object = new DataObject("dog");
+					ObjectController c = new ObjectController(object, null);
+
+					ObjectGUI og = new ObjectGUI(c);
+					og.init();
+
+
+			}
+		};
+		
+		ListView<HBox> listView = makeHBox(sButtonClick, 1, resources.getString(NEW_ITEM), resources.getString(OBJECT_TITLE));
+
+		bp.setLeft(listView);
+	}
+	private ListView<HBox> makeHBox(EventHandler<ActionEvent> e, int n, String name, String title) {
+		ArrayList<HBox> list = new ArrayList<HBox>();
+		HBox header = new HBox();
+		Label headerLabel = new Label(title);
+		header.getChildren().addAll(headerLabel);
+		list.add(header);
+		for (int i = 0; i < n; i++) {
+           // list.add(new HBoxCell("Item " + i, "Button " + i));
+			Button plus = new Button(" + ");
+			plus.setOnAction(e);
+			Label label = new Label(name);
+			HBox hbox = new HBox();
+		
+            label.setMaxWidth(Double.MAX_VALUE);
+            HBox.setHgrow(label, Priority.ALWAYS);
+            
+            hbox.getChildren().addAll(label, plus);
+            list.add(hbox);
+		}
+
+		ListView<HBox> listView = new ListView<HBox>();
+		ObservableList<HBox> myObservableList = FXCollections.observableList(list);
+		listView.setItems(myObservableList);
+		return listView;
+	}
+}
--- src/authoring_environment/View.java
+++ src/authoring_environment/View.java
@@ -73,9 +73,13 @@ public class View implements Observer{
 	private Stage myStage;
 	private Group myRoot;
 	private Controller myController;
-	private RoomEditor editor;
+	private RoomEditor RoomEditor;
 	private double ViewWidth;
 	private double ViewHeight;
+	private ObjectListWindow myObjectListView;
+	private RoomListView myRoomListView;
+	private RightWindowView myRightWindowView;
+	private TopMenuBar myTopToolBar;
 	private static String WIDTH = "ViewWidth";
 	private static String HEIGHT = "ViewHeight";
 	private static String OBJECT_TITLE = "ObjectListTitle";
@@ -96,16 +100,19 @@ public class View implements Observer{
 		myStage = new Stage();
 		myRoot = new Group();
 		myResourceBundle = resources;
-		
+		myObjectListView = new ObjectListWindow();
+		myRoomListView = new RoomListView();
+		myTopToolBar = new TopMenuBar();
+		myRightWindowView = new RightWindowView();
 		
 	}
 	public void init(){
 		BorderPane bp = new BorderPane();
 
-		makeCenterSpace(bp);
-		makeToolBar(bp);
-		addObjectWindow(bp);
-		makeRightWindow(bp);
+		myObjectListView.init(bp, myStage, myResourceBundle);
+		myRoomListView.init(bp, myStage, myResourceBundle);
+		myTopToolBar.init(bp, myResourceBundle, this);
+		myRightWindowView.init(bp, myResourceBundle, new VBox());
 		
 		int width = Integer.parseInt(myResourceBundle.getString(WIDTH));
 		int height = Integer.parseInt(myResourceBundle.getString(HEIGHT));
@@ -113,177 +120,15 @@ public class View implements Observer{
 		myStage.setScene(s);
 		myStage.show();
 	}
-	private void makeToolBar(BorderPane bp) {
-		Button Load = new Button(myResourceBundle.getString(LOAD));
-		Button Save = new Button(myResourceBundle.getString(SAVE));
-		Button Edit = new Button("Edit View");
-		Edit.setOnAction(new EventHandler<ActionEvent>(){
-	    	 @Override
-	    	 public void handle(ActionEvent event){
-	    		 addViewInput();
-	    	 }
-		});
-		ToolBar toolBar = new ToolBar(Load, Save, Edit );
-		bp.setTop(toolBar);
 
 
+	public void setViewHeight(double height){
+		ViewHeight = height;
 	}
-	private void makeRightWindow(BorderPane bp){
-
-		VBox rightWindow = new VBox();
-		addSoundWindow(rightWindow);
-		addSpriteWindow(rightWindow);
-		bp.setRight(rightWindow);
-
+	public void setViewWidth(double width){
+		ViewWidth = width;
 	}
-	private void makeCenterSpace(BorderPane bp) {
-		GridPane RoomView = new GridPane();
-		RoomView.setVgap(10);
-		RoomView.setHgap(20);
-		Button plus = new Button(" + ");
 
-		plus.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event){
-				final Stage dialog = new Stage();
-				ResourceBundle resources = ResourceBundle.getBundle("resources/RoomResources");
-				editor = new RoomEditor(resources);
-			}
-		});
-
-		RoomView.add(plus, 1, 1);
-
-		bp.setCenter(RoomView);
-	}
-	private void addObjectWindow(BorderPane bp){
-		EventHandler<ActionEvent> sButtonClick = new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event){
-				 	final Stage dialog = new Stage();
-					dialog.initModality(Modality.APPLICATION_MODAL);
-					dialog.initOwner(myStage);
-					VBox dialogVbox = new VBox(20);
-					dialogVbox.getChildren().add(new Text("This is a Dialog"));
-					Scene dialogScene = new Scene(dialogVbox, 300, 200);
-					dialog.setScene(dialogScene);
-					dialog.show();
-
-
-			}
-		};
-		
-		ListView<HBox> listView = makeHBox(sButtonClick, 1, myResourceBundle.getString(NEW_ITEM), myResourceBundle.getString(OBJECT_TITLE));
-
-		bp.setLeft(listView);
-	}
-	private void addSpriteWindow(VBox V){
-		EventHandler<ActionEvent> sButtonClick = new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event){
-				System.out.println("hey");
-			}
-		};
-		makeListView(V, sButtonClick, 1, myResourceBundle.getString(NEW_ITEM), myResourceBundle.getString(SPRITE_TITLE));
-	}
-	private void addSoundWindow(VBox V){
-		EventHandler<ActionEvent> sButtonClick = new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event){
-				System.out.println("hey");
-			}
-		};
-		
-		makeListView(V, sButtonClick, 1, myResourceBundle.getString(NEW_ITEM), myResourceBundle.getString(SOUND_TITLE));
-	}
-	private void makeListView(VBox V, EventHandler<ActionEvent> e, int n, String name, String title) {
-		ListView<HBox> listView = makeHBox(e, n, name, title);
-		
-		V.getChildren().add(listView);
-	}
-	private void addViewInput(){
-
-		 Dialog<Pair<String, String>> dialog = new Dialog<>();
-		 dialog.setTitle("Set height and width for view");
-
-		    // Set the button types.
-		 ButtonType loginButtonType = new ButtonType("OK", ButtonData.OK_DONE);
-		 dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
-
-		 GridPane gridPane = new GridPane();
-		 gridPane.setHgap(10);
-		 gridPane.setVgap(10);
-		 gridPane.setPadding(new Insets(20, 150, 10, 10));
-
-		 TextField from = new TextField();
-		 from.setPromptText("Height");
-		 TextField to = new TextField();
-		 to.setPromptText("Width");
-		 gridPane.add(new Label("Height:"), 0, 0);
-		 gridPane.add(from, 1, 0);
-		 gridPane.add(new Label("Width:"), 0, 1);
-		 gridPane.add(to, 1, 1);
-
-		 dialog.getDialogPane().setContent(gridPane);
-
-		    // Request focus on the username field by default.
-		 Platform.runLater(() -> from.requestFocus());
-
-		    // Convert the result to a username-password-pair when the login button is clicked.
-		 dialog.setResultConverter(dialogButton -> {
-		     if (dialogButton == loginButtonType) {
-		          return new Pair<>(from.getText(), to.getText());
-		     }
-	        return null;
-		 });
-
-		 Optional<Pair<String, String>> result = dialog.showAndWait();
-
-		 result.ifPresent(pair -> {
-		     setViewSize(pair.getKey(),  pair.getValue());
-		 });
-	}
-	private void setViewSize(String height, String width) {
-		boolean check = true;
-		try  
-		  {  
-		    double h = Double.parseDouble(height);  
-		    double w = Double.parseDouble(width);
-		  }  
-		  catch(NumberFormatException nfe)  
-		  {  
-		      System.out.println("not a number");
-		      check = false;
-		  }  
-		if(check){
-		  ViewHeight = Double.parseDouble(height);  
-		  ViewWidth = Double.parseDouble(width);
-		}
-	}
-	private ListView<HBox> makeHBox(EventHandler<ActionEvent> e, int n, String name, String title) {
-		ArrayList<HBox> list = new ArrayList<HBox>();
-		HBox header = new HBox();
-		Label headerLabel = new Label(title);
-		header.getChildren().addAll(headerLabel);
-		list.add(header);
-		for (int i = 0; i < n; i++) {
-           // list.add(new HBoxCell("Item " + i, "Button " + i));
-			Button plus = new Button(" + ");
-			plus.setOnAction(e);
-			Label label = new Label(name);
-			HBox hbox = new HBox();
-		
-            label.setMaxWidth(Double.MAX_VALUE);
-            HBox.setHgrow(label, Priority.ALWAYS);
-            
-            hbox.getChildren().addAll(label, plus);
-            list.add(hbox);
-		}
-
-		ListView<HBox> listView = new ListView<HBox>();
-		ObservableList<HBox> myObservableList = FXCollections.observableList(list);
-		listView.setItems(myObservableList);
-		return listView;
-	}
 	private void updateObjectList(){
 		
 	}
