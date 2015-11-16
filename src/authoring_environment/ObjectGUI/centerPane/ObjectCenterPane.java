40325a0e8bbf80fb28c50bbd4725ab2f33095785
Parit Burintrathikul <aiprit@gmail.com>
2015-11-15 23:53:18 -0500

    fix the fking ratios for centerpane

--- src/authoring_environment/ObjectGUI/centerPane/ObjectCenterPane.java
+++ src/authoring_environment/ObjectGUI/centerPane/ObjectCenterPane.java
@@ -8,7 +8,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -17,27 +16,21 @@ public class ObjectCenterPane {
 	private ResourceBundle r = ResourceBundle.getBundle("authoring_environment/ObjectGUI/centerPane/CenterPaneResources");
 	private ObjectController myController;
 	private	Image sprite;
-	private ImageView spriteview;
-	private double xPos, yPos,spriteWidth ,spriteHeight;
+	private double xPos, yPos,spriteWidth,spriteHeight;
 
 	public Group init(ObjectController controller) {
 		myController = controller;
 		Group root = new Group();
 		Canvas c = new Canvas(Integer.parseInt(r.getString("canvasWidth")), Integer.parseInt(r.getString("canvasHeight")));
 		GraphicsContext gc = c.getGraphicsContext2D();
-		double a= Double.parseDouble(r.getString("defaultSize"));
 		try {
 			sprite = addSprite(myController.getCurrentSprite().getName());
-
 		}
 		catch (NullPointerException e) {
 			sprite = addSprite("Mario.png");
 		}
-		spriteview = new ImageView(sprite);
-		spriteview.setFitWidth(a);
-		spriteview.setPreserveRatio(true);
-		spriteWidth = spriteview.getBoundsInParent().getWidth()/spriteview.getImage().getWidth();;
-		spriteHeight =  spriteview.getBoundsInParent().getHeight()/spriteview.getImage().getHeight();
+		spriteWidth =sprite.getWidth();
+		spriteHeight =sprite.getHeight();
 		xPos = Integer.parseInt(r.getString("canvasWidth"))/2 - myController.getSize()[0]/2*spriteWidth;
 		yPos = Integer.parseInt(r.getString("canvasHeight"))/2 - myController.getSize()[1]/2*spriteHeight;
 		gc.drawImage(sprite, xPos, yPos, myController.getSize()[0]*spriteWidth, myController.getSize()[1]*spriteHeight);
@@ -56,6 +49,6 @@ public class ObjectCenterPane {
 
 
 	private Image addSprite(String s) {
-		return new Image(getClass().getClassLoader().getResourceAsStream(s), myController.getSize()[0], myController.getSize()[1], false, false);
+		return new Image(getClass().getClassLoader().getResourceAsStream(s));
 	}
 }
