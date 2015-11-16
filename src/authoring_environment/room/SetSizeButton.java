27bd314801031ca5e52fe7ab137486c4348111da
Elizabeth Dowd <med61@duke.edu>
2015-11-15 22:54:46 -0500

    View dragging bounds are set

--- src/authoring_environment/room/SetSizeButton.java
+++ src/authoring_environment/room/SetSizeButton.java
@@ -5,7 +5,10 @@ import java.util.ResourceBundle;
 public class SetSizeButton extends RoomEditorButton {
 	private static final String SIZE_TITLE = "SizeTitle";
 	
-	public SetSizeButton(ResourceBundle resources) {
+	private RoomBackground myBackground;
+	
+	public SetSizeButton(ResourceBundle resources, RoomBackground background) {
 		super(resources, SIZE_TITLE);
+		myBackground = background;
 	}
 }
