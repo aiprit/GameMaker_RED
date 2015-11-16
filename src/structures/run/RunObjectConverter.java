a439229a27147ecf3ca43f84d99bc183559fe906
Austin McKee <amckee3@gmail.com>
2015-11-16 03:10:23 -0500

    Run Object Converter

---
+++ src/structures/run/RunObjectConverter.java
@@ -0,0 +1,10 @@
+package structures.run;
+
+import structures.data.DataObject;
+
+public class RunObjectConverter {
+	public static RunObject convert(DataObject data) {
+		RunObject run = new RunObject(data.getName());
+		return null;
+	}
+}
