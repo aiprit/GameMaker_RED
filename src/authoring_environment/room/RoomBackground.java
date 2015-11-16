9a0e0d197b4b0ac2e0d4edcadf3ac9a15641657a
Elizabeth Dowd <med61@duke.edu>
2015-11-16 00:13:47 -0500

    Changed RoomBackground to canvas

--- src/authoring_environment/room/RoomBackground.java
+++ src/authoring_environment/room/RoomBackground.java
@@ -2,12 +2,12 @@ package authoring_environment.room;
 
 import java.util.ResourceBundle;
 
+import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Rectangle;
 
-public class RoomBackground extends Rectangle {
+public class RoomBackground extends Canvas {
 	public static final Color DEFAULT_COLOR = Color.WHITE;
 	
 	private Color myColor;
@@ -18,7 +18,17 @@ public class RoomBackground extends Rectangle {
 		super(Double.parseDouble(resources.getString("PreviewWidth")), 
 				Double.parseDouble(resources.getString("PreviewHeight"))-1);
 		myColor = DEFAULT_COLOR;
-		this.setFill(DEFAULT_COLOR);
+		setColorFill(DEFAULT_COLOR);
+	}
+
+	private void setColorFill(Color fill) {
+		this.getGraphicsContext2D().setFill(fill);
+		this.getGraphicsContext2D().fillRect(0, 0, this.getWidth(), this.getHeight());
+	}
+	
+	private void setImageFill(Image image) {
+		this.getGraphicsContext2D().setFill(new ImagePattern(image));
+		this.getGraphicsContext2D().fillRect(0, 0, this.getWidth(), this.getHeight());
 	}
 
 	public Color getColor() {
@@ -44,17 +54,17 @@ public class RoomBackground extends Rectangle {
 	//TODO write this to IRoom as well
 	public void setBackground(Color color, Image image, String fileName) {
 		if (image != null) {
-			this.setFill(new ImagePattern(image));
+			setImageFill(image);
 			myImage = image;
 			myImageFileName = fileName;
 			myColor = null;
 		} else if (color != null){
-			this.setFill(color);
+			setColorFill(color);
 			myColor = color;
 			myImage = null;
 			myImageFileName = null;
 		} else {
-			this.setFill(DEFAULT_COLOR);
+			setColorFill(DEFAULT_COLOR);
 			myColor = DEFAULT_COLOR;
 			myImage = image;
 			myImageFileName = fileName;
