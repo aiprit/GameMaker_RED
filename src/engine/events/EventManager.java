57aa971470c7f8364702d51584c5bb3b52bffee1
BaileyEliz <bew21@duke.edu>
2015-11-18 02:58:27 -0500

    made event listeners lists

--- src/engine/events/EventManager.java
+++ src/engine/events/EventManager.java
@@ -1,71 +1,87 @@
 package engine.events;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-import engine.IDraw;
-import javafx.scene.image.Image;
 import javafx.scene.input.InputEvent;
 import structures.run.RunRoom;
-import structures.run.RunView;
 
 public class EventManager implements IGUIHandler, IRoomChangedHandler, IGamePlayHandler {
-	
-	private IGUIHandler myGUI;
+
+	private List<IGUIHandler> myGUI;
 	private List<IRoomChangedHandler> myRoomChanged;
-	private IGamePlayHandler myUserInput;
-	
+	private List<IGamePlayHandler> myUserInput;
+
 	public EventManager(){
+		myGUI = new ArrayList<IGUIHandler>();
 		myRoomChanged = new ArrayList<IRoomChangedHandler>();
+		myUserInput = new ArrayList<IGamePlayHandler>();
 	}
-	
-	public void setGUIInterface(IGUIHandler gui){
-		myGUI = gui;
+
+	public void addGUIInterface(IGUIHandler gui){
+		myGUI.add(gui);
 	}
-	
+
 	public void addRoomChangedInterface(IRoomChangedHandler roomChanged){
 		myRoomChanged.add(roomChanged);
 	}
-	
+
 	public void addUserInputInterface(IGamePlayHandler userInput){
-		myUserInput = userInput;
+		myUserInput.add(userInput);
 	}
-	
+
 	public void onReset(){
-		myGUI.onReset();
+		for(IGUIHandler g : myGUI){
+			g.onReset();
+		}
 	}
-	
+
 	public void onStart(){
-		myGUI.onStart();
+		for(IGUIHandler g : myGUI){
+			g.onStart();
+		}
 	}
-	
+
 	public void onPause(){
-		myGUI.onPause();
+		for(IGUIHandler g : myGUI){
+			g.onPause();
+		}
 	}
-	
+
 	public void onLoadSave(String path){
-		myGUI.onLoadSave(path);
+		for(IGUIHandler g : myGUI){
+			g.onLoadSave(path);
+		}
 	}
-	
+
 	public void onSave(){
-		myGUI.onSave();
+		for(IGUIHandler g : myGUI){
+			g.onSave();
+		}
 	}
-	
+
 	public void onRoomChanged(RunRoom runRoom){
-		for(IRoomChangedHandler roomHandler : myRoomChanged){
-			roomHandler.onRoomChanged(runRoom);
+		for(IRoomChangedHandler r : myRoomChanged){
+			r.onRoomChanged(runRoom);
 		}
 	}
 
 	@Override
 	public void setOnEvent(InputEvent m) {
-		myUserInput.setOnEvent(m);
+		for(IGamePlayHandler i : myUserInput){
+			i.setOnEvent(m);
+		}
 	}
 
 	@Override
 	public Queue<InputEvent> getEvents() {
-		return myUserInput.getEvents();
+		Queue<InputEvent> currentEvents = new LinkedList<>();
+		for(IGamePlayHandler i : myUserInput){
+			currentEvents.addAll(i.getEvents());
+		}
+		return currentEvents;
 	}
 
 }
