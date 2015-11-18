108357a6cd3e0f97d86821c8a58d5554b5177625
buie <buie.andrew@gmail.com>
2015-11-18 03:52:02 -0500

    sprites

--- src/XML/XMLWriter.java
+++ src/XML/XMLWriter.java
@@ -2,8 +2,10 @@ package XML;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import structures.data.DataGame;
 import structures.data.DataObject;
+import structures.data.DataSprite;
 import structures.data.actions.IAction;
 import structures.data.events.IDataEvent;
 
@@ -34,9 +36,8 @@ public class XMLWriter {
             root.setAttribute("title", game.getName());
             root.setAttribute("directory", game.getGameDirectory());
 
-            root.appendChild(writeDataObjects(doc, game));
-
-
+            root.appendChild(getElementFromDataObjects(doc, game));
+            root.appendChild(getElementFromSprites(doc, game));
 
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
@@ -54,40 +55,59 @@ public class XMLWriter {
         }
     }
 
-    private Element writeDataObjects(Document doc, DataGame game){
+    private Element getElementFromDataObjects(Document doc, DataGame game){
         Element objects = doc.createElement("objects");
         for(DataObject dataObject : game.getObjects()){
-            objects.appendChild(generateElementFromObject(doc, dataObject));
+            objects.appendChild(getElementFromObject(doc, dataObject));
         }
         return objects;
     }
 
-    private Element generateElementFromObject(Document doc, DataObject dataObject){
+    private Element getElementFromObject(Document doc, DataObject dataObject){
         Element object = doc.createElement("object");
         object.setAttribute("name", dataObject.getName());
         object.setAttribute("zIndex", Integer.toString(dataObject.getZIndex()));
         object.setAttribute("sprite", dataObject.getSprite().getName());
 
         for (Map.Entry<IDataEvent, List<IAction>> e : dataObject.getEvents().entrySet()) {
-            object.appendChild(generateElementFromEvent(doc, e));
+            object.appendChild(getElementFromEvent(doc, e));
         }
         return object;
     }
 
-    private Element generateElementFromEvent(Document doc, Map.Entry<IDataEvent, List<IAction>> e){
+    private Element getElementFromEvent(Document doc, Map.Entry<IDataEvent, List<IAction>> e){
         Element event = doc.createElement("event");
         event.setAttribute("title", e.getKey().getName());
 
         for(IAction a : e.getValue()){
-            event.appendChild(generateElementFromAction(doc, a));
+            event.appendChild(getElementFromAction(doc, a));
         }
 
         return event;
     }
 
-    private Element generateElementFromAction(Document doc, IAction a){
+    private Element getElementFromAction(Document doc, IAction a){
         Element action = doc.createElement("action");
         action.setAttribute("title", a.getTitle());
         return action;
+    }
+
+    private Element getElementFromSprites(Document doc, DataGame game){
+        Element sprites = doc.createElement("sprites");
+        for(DataSprite dataSprite : game.getSprites()){
+            sprites.appendChild(getElementFromSprite(doc, dataSprite));
+        }
+        return sprites;
+    }
+
+    private Element getElementFromSprite(Document doc, DataSprite dataSprite) {
+        Element sprite = doc.createElement("sprite");
+        sprite.setAttribute("name", dataSprite.getName());
+        sprite.setAttribute("baseFileName", dataSprite.getBaseFileName());
+        sprite.setAttribute("centerX", Double.toString(dataSprite.getCenterX()));
+        sprite.setAttribute("centerY", Double.toString(dataSprite.getCenterY()));
+        sprite.setAttribute("scaleX", Double.toString(dataSprite.getScaleX()));
+        sprite.setAttribute("scaleY", Double.toString(dataSprite.getScaleY()));
+        return sprite;
     }
 }
