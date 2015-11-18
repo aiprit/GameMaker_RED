5fd03d9f590efd8bb92eeaaeb8211fc4c9dfe1e3
Parit Burintrathikul <aiprit@gmail.com>
2015-11-17 20:21:35 -0500

    added toString to show listview

--- src/structures/data/events/AbstractBasicEvent.java
+++ src/structures/data/events/AbstractBasicEvent.java
@@ -1,12 +1,12 @@
 package structures.data.events;
 
 public abstract class AbstractBasicEvent implements IDataEvent {
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		return this.getClass().equals(obj.getClass());
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return this.getClass().hashCode();
