596e9c13d7121c1e222630b7892b011fb9fef6a1
Elizabeth Dowd <med61@duke.edu>
2015-11-25 12:33:58 -0500

    Adjusted Launcher so that authoring environment is centered when opened

--- src/authoring_environment/main/MainController.java
+++ src/authoring_environment/main/MainController.java
@@ -33,8 +33,8 @@ public class MainController implements IUpdateHandle {
 	private SoundListView soundListView;
 	private TopMenuBar topMenuBar;
 
-	public MainController(Stage myStage) {
-		this.myStage = myStage;
+	public MainController() {
+		this.myStage = new Stage();
 		mainView = new MainView(myStage);
 		objectListWindow = new ObjectListWindow();
 		roomListView = new RoomListView();
