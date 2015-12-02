12c478a1780fec531f4add9dcd59c44284558cde
Elizabeth Dowd <med61@duke.edu>
2015-12-01 23:15:56 -0500

    Room names must be unique and not null

---
+++ src/authoring_environment/room/error/ErrorPopup.java
@@ -0,0 +1,36 @@
+package authoring_environment.room.error;
+
+import java.util.ResourceBundle;
+
+import authoring_environment.room.PopupTemplate;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+public class ErrorPopup extends PopupTemplate {
+	private static final int INSETS_SPACING = 10;
+	private static final int LINE_SPACING = 5;
+	private static final String OKAY = "Okay";
+	
+	private Text myText;
+	
+	public ErrorPopup(ResourceBundle resources, String title, String errorMessage) {
+		super(resources, title);
+		super.getSaveButton().setText(resources.getString(OKAY));
+		super.getSaveButton().setOnAction(e -> this.close());
+		super.setAlwaysOnTop(true);
+		myText.setText(errorMessage);
+	}
+
+	@Override
+	protected void setContents() {
+		myText = new Text();
+		myText.setWrappingWidth(Double.parseDouble(super.getResources().getString(POPUP_WIDTH))-20);
+		myText.setTextAlignment(TextAlignment.CENTER);
+		myText.setLineSpacing(LINE_SPACING);
+		myContentsBox.getChildren().add(myText);
+		myContentsBox.setAlignment(Pos.CENTER);
+	}
+
+}
