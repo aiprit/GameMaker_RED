f9a4aa511254a6b320adce6af5f8e34fbc7658b9
BaileyEliz <bew21@duke.edu>
2015-12-02 03:04:03 -0500

    small util refactoring

--- src/controllerUtils/RobotEventFactory.java
+++ src/controllerUtils/RobotEventFactory.java
@@ -16,48 +16,52 @@ import javafx.scene.input.KeyCode;
 import net.java.games.input.Event;
 
 public class RobotEventFactory {
-	
+
 	private Properties xbox;
 	private Map<String, String> eventMap;
-	
+
 	public RobotEventFactory() throws IOException{
+
 		InputStream commands = this.getClass().getClassLoader()
-		.getResourceAsStream("controllerUtils/controllerAttributes.txt");
-		
+				.getResourceAsStream("controllerUtils/controllerAttributes.txt");
+
 		InputStreamReader is = new InputStreamReader(commands);
 		BufferedReader br = new BufferedReader(is);
 		List<String> components = new ArrayList<>();
 
 		String read = br.readLine();
 		while(read != null){
+			//ignore the comments in the text file
 			if(read.contains("//")){
 				read = read.substring(0, read.indexOf(" //"));
 			}
 			components.add(read);
 			read = br.readLine();
 		}
-		
+
 		InputStream s = this.getClass().getClassLoader()
-        .getResourceAsStream("controllerUtils/xbox.properties");
+				.getResourceAsStream("controllerUtils/xbox.properties");
 		xbox = new Properties();
 		xbox.load(s);
 		eventMap = new HashMap<String, String>();
-		
+
 		for(String component : components){
 			eventMap.put(component, xbox.getProperty(component));
 		}
 	}
-	
+
 	public void executeEvent(FXRobot robot, Event event){
-		
+
 		if(eventMap.containsKey(event.getComponent().getName())){
-			if(eventMap.get(event.getComponent().getName()).contains("Key Press")){
-				KeyCode kc = KeyCode.valueOf(eventMap.get(event.getComponent().getName()).substring(10));
-				robot.keyPress(kc);
-				robot.keyRelease(kc);
+			if(eventMap.get(event.getComponent().getName()) == null ||
+				eventMap.get(event.getComponent().getName()).equals("")){
+				return;
 			}
+			KeyCode kc = KeyCode.valueOf(eventMap.get(event.getComponent().getName()));
+			robot.keyPress(kc);
+			robot.keyRelease(kc);
 		}
-		
+
 	}
 
 }
