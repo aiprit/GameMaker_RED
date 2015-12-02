534ce7e68ade62adf117901c7ca7de3eaf3dfdfa
Ankit Kayastha <ankit.kayastha@duke.edu>
2015-12-01 23:16:47 -0500

    Reverted back to old HelpIcon code

--- src/authoring_environment/room/button_toolbar/HelpIcon.java
+++ src/authoring_environment/room/button_toolbar/HelpIcon.java
@@ -7,11 +7,9 @@ import java.io.FileReader;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
-import javafx.geometry.Point2D;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.PopupWindow.AnchorLocation;
 
 public class HelpIcon extends ImageView {	
@@ -23,19 +21,8 @@ public class HelpIcon extends ImageView {
 		super(image);
 		myResources = resources;
 		initializeTooltip();
-		this.setOnMouseClicked(e -> showTooltip(e, myTooltip));
+		this.setOnMouseClicked(e -> myTooltip.show(this, e.getScreenX(), e.getScreenY()));
 		this.setOnMouseExited(e -> myTooltip.hide());
-	}
-	
-	private void showTooltip(MouseEvent event, Tooltip tip) {
-		//System.out.println("Screen X is " + event.getScreenX());
-		//System.out.println("Screen Y is " + event.getScreenY());
-		Point2D point = this.screenToLocal(event.getScreenX(), event.getScreenY());
-		double xCor = point.getX();
-		double yCor = point.getY();
-		System.out.println("X Cor is " + xCor);
-		System.out.println("Y Cor is " + yCor);
-		tip.show(this, xCor, yCor);
 	}
 	
 	private void initializeTooltip() {
@@ -43,8 +30,8 @@ public class HelpIcon extends ImageView {
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(tooltipTextFile));
 	        String tooltipText = bufferedReader.lines().map(e -> e).collect(Collectors.joining("\n"));
-	        myTooltip = new Tooltip(tooltipText);
-			//myTooltip.setAnchorLocation(AnchorLocation.CONTENT_BOTTOM_RIGHT);
+			myTooltip = new Tooltip(tooltipText);
+			myTooltip.setAnchorLocation(AnchorLocation.CONTENT_BOTTOM_RIGHT);
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
