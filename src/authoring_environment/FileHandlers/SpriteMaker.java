399860caffd85b2e9bb837b956d99a35ba126987
rooper <logan.rooper@gmail.com>
2015-12-01 23:18:06 -0500

    spriteMaker

--- src/authoring_environment/FileHandlers/SpriteMaker.java
+++ src/authoring_environment/FileHandlers/SpriteMaker.java
@@ -9,6 +9,7 @@ import java.util.ResourceBundle;
 import javax.imageio.ImageIO;
 
 import exceptions.ResourceFailedException;
+import game_file_manager.GameFileManager;
 import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -26,84 +27,60 @@ import structures.data.DataSprite;
 
 public class SpriteMaker {
 	private static ResourceBundle r = ResourceBundle.getBundle("resources/EnvironmentGUIResources");
-	public static void load(Stage s, ObservableList<DataSprite> sprites){
+
+	public static void load(Stage s, ObservableList<DataSprite> sprites) {
 		FileChooser fileChooser = new FileChooser();
-		fileChooser.setTitle("fileChooserTitle");
-		
+		GameFileManager gfm = new GameFileManager();
+		fileChooser.setTitle(r.getString("fileChooserTitle"));
+
 		File selectedFile = fileChooser.showOpenDialog(s);
-		BufferedImage img;
-		
-		try {
-			try{
-			img = ImageIO.read(selectedFile);
-			String name = askName();
-			File outputfile = new File(name + ".png");
-			
-		    ImageIO.write(img, "png", outputfile);
-		    DataSprite newSprite = new DataSprite(name, outputfile.getName());
 
-		    try {
-				newSprite.load(r.getString("imagesFolder"));
-			} catch (ResourceFailedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		  
-		    sprites.add(newSprite);
-			} catch(IllegalArgumentException e2){
-				
-			}
-			
-		   
-		    
-		    
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			
+		Optional<DataSprite> ds = gfm.makeSprite(selectedFile, askName());
+		if (ds.isPresent()) {
+			sprites.add(ds.get());
+		} else {
+			// Try again
+			load(s, sprites);
 		}
-		
 	}
-	public static void show(DataSprite sprite){
+
+	public static void show(DataSprite sprite) {
 		BorderPane myPane = new BorderPane();
 		Stage s = new Stage();
 		BufferedImage img = sprite.getImage();
 		WritableImage wr = null;
-        if (img != null) {
-            wr = new WritableImage(img.getWidth(), img.getHeight());
-            PixelWriter pw = wr.getPixelWriter();
-            for (int x = 0; x < img.getWidth(); x++) {
-                for (int y = 0; y < img.getHeight(); y++) {
-                    pw.setArgb(x, y, img.getRGB(x, y));
-                }
-            }
-        }
+		if (img != null) {
+			wr = new WritableImage(img.getWidth(), img.getHeight());
+			PixelWriter pw = wr.getPixelWriter();
+			for (int x = 0; x < img.getWidth(); x++) {
+				for (int y = 0; y < img.getHeight(); y++) {
+					pw.setArgb(x, y, img.getRGB(x, y));
+				}
+			}
+		}
 		Canvas c = new Canvas(img.getWidth(), img.getHeight());
 		GraphicsContext centerGC = c.getGraphicsContext2D();
-	
 
-
-		double xPos = img.getWidth()/2;
-		double yPos = img.getHeight()/2;
-		centerGC.drawImage(wr, 0, 0); 
+		double xPos = img.getWidth() / 2;
+		double yPos = img.getHeight() / 2;
+		centerGC.drawImage(wr, 0, 0);
 
 		myPane.setCenter(c);
 
 		s.setScene(new Scene(myPane));
 		s.show();
 	}
-	private static String askName(){
+
+	private static String askName() {
 		TextInputDialog dialog = new TextInputDialog("Sprite Name");
 		dialog.setTitle("Sprite Selector");
 		dialog.setHeaderText("Sprite");
 		dialog.setContentText("Please enter your sprite name:");
 		Optional<String> result = dialog.showAndWait();
-		if (result.isPresent()){
-		    return result.get();
-		}
-		else 
+		if (result.isPresent()) {
+			return result.get();
+		} else
 			return "IMAGE";
-		
-		
+
 	}
 }
---
+++ src/game_file_manager/GameFileManager.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package game_file_manager;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+
+import exceptions.ResourceFailedException;
+import javafx.scene.image.Image;
+import javafx.scene.media.AudioClip;
+import structures.data.DataSprite;
+
+/**
+ * @author loganrooper Handles read and writing resources If it cannot write a
+ *         file (Naming conflict, file system issue), set methods return false.
+ */
+public class GameFileManager {
+	private static ResourceBundle r = ResourceBundle.getBundle("resources/EnvironmentGUIResources");
+
+	public GameFileManager() {
+
+	}
+
+	public Boolean initializeGameFileStructure() {
+		// Check if the game already exists
+		return false;
+	}
+
+	public Boolean renameGame(String gameName) {
+		return false;
+	}
+
+	public Optional<Image> getSprite(String imageName) {
+		return null;
+	}
+
+	public Optional<AudioClip> getSound(String soundName, String name) {
+		return null;
+	}
+
+	public Optional<DataSprite> makeSprite(File image, String name) {
+		DataSprite newSprite;
+		
+		try {
+			BufferedImage img = ImageIO.read(image);
+			File localFile = new File(name + ".png");
+
+			ImageIO.write(img, "png", localFile);
+			newSprite = new DataSprite(name, localFile.getName());
+			newSprite.load(r.getString("imagesFolder"));
+		} catch (ResourceFailedException e) {
+			return Optional.empty();
+		} catch (IOException e) {
+			return Optional.empty();
+		}
+		
+		return Optional.of(newSprite);
+	}
+
+	public Optional<AudioClip> makeSound(File audioFile) {
+		return null;
+	}
+}
