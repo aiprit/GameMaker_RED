83006e2b38f836d9129e0ee3a94131e6d5a748f5
BaileyEliz <bew21@duke.edu>
2015-12-06 00:54:02 -0500

    few more actions

--- src/structures/data/actions/game/SetTimerRepeated.java
+++ src/structures/data/actions/game/SetTimerRepeated.java
@@ -1,29 +1,37 @@
 package structures.data.actions.game;
 
 import structures.data.DataAction;
+import structures.data.actions.params.IntegerParam;
 
+/**
+ * AUSTIN MCKEE STAY AWAY FROM THIS CODE
+ * @author baileyewall
+ *
+ */
 public class SetTimerRepeated extends DataAction {
 
 	public SetTimerRepeated(){
-		init();
+		init(new IntegerParam("DelayTimerTime"), new IntegerParam("PeriodTimerTime"));
 	}
 
 	@Override
 	public String getTitle() {
-		// TODO Auto-generated method stub
 		return "SetTimerRepeated";
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return "Set Timer Repeated";
+		return String.format("executes the original code repeatedly after %d milliseconds with %d milliseconds in between", get("DelayTimerTime").getValue(), get("PeriodTimerTime").getValue());
 	}
 
 	@Override
 	protected String getSyntax() {
 		// TODO Auto-generated method stub
-		return null;
+		return "}\n"
+				+ "int delay = %d\n"
+				+ "int period = %d\n"
+				+ "Timer timer = new Timer()\n"
+				+ "timer.scheduleAtFixedRate(new RepeatedTimerTask(), delay, period)";
 	}
 
 }
